fda0a7b7d8953b62676ba9b905f68f7b59fe4ba6
gorchels <gorchels@bren.ucsb.edu>
2021-03-15 17:25:38 -0400

    Working on the code

--- StarUtility/app.R
+++ StarUtility/app.R
@@ -18,7 +18,7 @@ library(shinydashboard)
 
 utility <- read_csv("data/example.csv") %>% 
   clean_names() %>% 
-  mutate(production=al_well_1+al_well_2+al_well_3+al_well_4+post_reservoir_meter+canyon_lake) %>% 
+  mutate(production=al_well_1+al_well_2+al_well_3+al_well_4+post_reservoir_meter+canyon_lake+wur_well_1+wur_well_2+wur_well_3+wur_well_4+wur_well_5+wur_well_6) %>% 
   mutate(date=as.Date(date)) %>% 
   mutate(wur=wur_well_1+wur_well_2+wur_well_3+wur_well_4+wur_well_5+wur_well_6) %>% 
   mutate(asp = al_well_1+al_well_2+al_well_3+al_well_4)
@@ -38,21 +38,48 @@ ui <- fluidPage(
                         img(src= "star_smaller.png", align = "center")
                       )
                       
-             ),
+             ), 
+  
+tabPanel("Hello Shiny!",
+
+sidebarLayout(
+  
+  # Sidebar with a slider input
+  sidebarPanel(
+    sliderInput("obs",
+                "Number of observations:",
+                min = 0,
+                max = 1000,
+                value = 500)
+  ),
+  
+  # Show a plot of the generated distribution
+  mainPanel(
+    plotOutput("distPlot")
+  )
+)))
+
+
+# Server logic
+server <- function(input, output) {
+  output$distPlot <- renderPlot({
+    hist(rnorm(input$obs))
+  })
+}
   
 #)
              
-             tabPanel("Daily Participation",
+  #           tabPanel("Daily Participation",
                       #sidebar with input widgets
-                      sidebarLayout(
-                        sidebarPanel(
-                         dateRangeInput("date_range",
-                                        label = "Select Date Range",
-                                         start = "2012-01-01",
-                                         end = "2012-12-31",
-                                         min = "2012-01-01",
-                                         max = "2012-12-31"))),
-                   #       ),
+     #                 sidebarLayout(
+     #                   sidebarPanel(
+     #                    dateRangeInput("date_range",
+      #                                  label = "Select Date Range",
+       #                                  start = "2012-01-01",
+      #                                   end = "2012-12-31",
+      #                                   min = "2012-01-01",
+        #                                 max = "2012-12-31"))),
+                  #       ),
                     #      checkboxGroupInput("time", 
                      #                        label = "Select Student Gender Preference",
                       #                       choices = list("Man" = "m", "Woman" = "w"),
@@ -60,40 +87,40 @@ ui <- fluidPage(
                         #  )
                    #     ),
                        # main panel
-                        mainPanel( 
-                          h2("Student Participation in Core Classes"),
-                          plotOutput("time_plot")
-                        )
+          #              mainPanel( 
+            #              h2("Student Participation in Core Classes"),
+            #              plotOutput("time_plot")
+            #            )
                       #)
                       
-             ))
+           #  ))
 
-server <- function(input, output) {
+#server <- function(input, output) {}
   
   #reaction for time graph widgets
-  datareact_time <- reactive({
-    utility %>% 
-      mutate(date = mdy(date))
-      select(date, production) %>% 
-      filter(date >= input$date_range[1], date <= input$date_range[2]) 
-  })
+ # datareact_time <- reactive({
+ #   utility %>% 
+ #     mutate(date = mdy(date))
+ #     select(date, production) %>% 
+  #    filter(date >= input$date_range[1], date <= input$date_range[2]) 
+ # })
   
   # time series panel
-  output$time_plot <- renderPlot(
-    {
-      ggplot(datareact_time(), aes(x = date, y=production))+
-        geom_line() +
+ # output$time_plot <- renderPlot(
+ #   {
+  #    ggplot(datareact_time(), aes(x = date))+
+  #      geom_line(aes(y=production)) +
        # scale_fill_manual(limits = c("m", "w"), values = c("royalblue4", "darkolivegreen4"), name = "Student Gender Preference", labels = c("Man", "Woman")) +
-        theme_classic() +
-        scale_y_continuous(expand = c(0,0), limits = c(0,80), breaks = seq(0,80, by = 5)) +
-        scale_x_date(breaks = as.Date(c("2012-01-01", "2012-04-01", "2012-07-01", "2012-10-01", "2012-12-31"))) +
-        theme(axis.text.x = element_text(angle = 90, hjust = 1))+
-        labs(x = "Date", y = "Production (MG)") 
-    }
-  )
+  #      theme_classic() +
+ #       scale_y_continuous(expand = c(0,0), limits = c(45,200), breaks = seq(45,200, by = 10)) +
+ #       scale_x_date(breaks = as.Date(c("2012-01-01", "2012-04-01", "2012-07-01", "2012-10-01", "2012-12-31"))) +
+  #      theme(axis.text.x = element_text(angle = 90, hjust = 1))+
+  #      labs(x = "Date", y = "Production (MG)") 
+  #  }
+  #)
   
-  
-}
+
+#}
 
 # Run the application 
 shinyApp(ui = ui, server = server)
